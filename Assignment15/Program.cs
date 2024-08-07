5b60a46a4ea6ba4172f0a5d3de81d38a5228bd02
invernoe <mahmoudk221999@gmail.com>
2024-08-07 22:11:29 +0300

    Add project files.

---
+++ Assignment15/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Assignment15.ListGenerators;
+
+namespace Assignment15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
